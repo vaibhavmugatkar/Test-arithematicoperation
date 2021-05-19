390dd23958fcf3a94bf13aa9af451e1fc69acc1f
vaibhavmugatkar <vaibhav.mugatkar@gmail.com>
2021-05-20 01:23:51 +0530

    [vaibhav] modified.sh

--- arithematic.sh
+++ arithematic.sh
@@ -1,1 +1,5 @@
 #! /bin/bash
+read -p "enter the number a" number a
+read -p "enter the number b" number b
+read -p "enter the number c" number c
+echo "number a number b number c" $numbera $numberb $numberc 
