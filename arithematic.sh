bb50f8f0df9f03109da9b66571b4a0da101ad3ec
vaibhavmugatkar <vaibhav.mugatkar@gmail.com>
2021-05-20 01:35:34 +0530

    [vaibhav] modified.sh

--- arithematic.sh
+++ arithematic.sh
@@ -3,3 +3,11 @@ read -p "enter the number a" number a
 read -p "enter the number b" number b
 read -p "enter the number c" number c
 echo "number a number b number c" $numbera $numberb $numberc 
+
+
+a=2
+b=3
+c=4
+add=(( a+b*c ))
+echo "add" $add
+
